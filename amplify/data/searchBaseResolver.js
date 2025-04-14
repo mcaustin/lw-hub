76510d93ac20b2b9e390b4d6ad07ba7c67eb7c25
mcaustin <mcaustin@toasttab.com>
2025-04-14 16:33:46 -0400

    api gateway

---
+++ amplify/data/searchBaseResolver.js
@@ -0,0 +1,37 @@
+import { util } from "@aws-appsync/utils";
+
+/**
+ * Searches for documents by using an input term
+ * @param {import('@aws-appsync/utils').Context} ctx the context
+ * @returns {*} the request
+ */
+
+export function request(ctx) {
+ 
+
+  return {
+    version: "2018-05-29",
+    method: "GET",
+    params: {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        q: ctx.args.q ,
+      },
+    },
+    //resourcePath: `/base/_search`,
+  };
+}
+/**
+ * Returns the fetched items
+ * @param {import('@aws-appsync/utils').Context} ctx the context
+ * @returns {*} the result
+ */
+
+export function response(ctx) {
+  const { statusCode, body } = ctx.result;
+  if (statusCode === 200) {
+    return JSON.parse(body).hits.hits.map((hit) => hit._source);
+  }
+}
